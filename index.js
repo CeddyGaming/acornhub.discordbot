8eef8a4a867f62361a5546e8357fed57213bdad5
CeddyGaming <cjroblox123@gmail.com>
2021-01-21 17:59:49 +0000

    Initial Commit

---
+++ index.js
@@ -0,0 +1,21 @@
+const Discord = require('discord.js')
+const fs = require('fs')
+const client = new Discord.Client()
+client.commands = new Discord.Collection()
+const {prefix, token} = require('./config.json')
+
+client.on('ready', () => {
+    console.log(`Client logged in as ${client.user}`)
+})
+
+client.on('message', (message) => {
+    //!ban @cedric will continue; ban @cedric will return
+    if(message.author.bot || !message.content.startsWith(prefix)) return
+    
+    //!ban 
+    //@cedricjake2006
+    const commandAndArguements = message.content.split(' ')
+
+})
+
+client.login(token)
