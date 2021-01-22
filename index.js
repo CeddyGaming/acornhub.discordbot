823695b6e1904790254b3b10f8d4bc99ab864d6b
CeddyGaming <cjroblox123@gmail.com>
2021-01-22 16:46:25 +0000

    EEEEE
    Signed-off-by: CeddyGaming <cjroblox123@gmail.com>

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
+//Constants
 const Discord = require('discord.js')
 const fs = require('fs')
+const {prefix, token} = require('./config.json')
+
+//Initalize
 const client = new Discord.Client()
 client.commands = new Discord.Collection()
-const {prefix, token} = require('./config.json')
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 client.on('ready', () => {
     console.log(`Client logged in as ${client.user}`)
@@ -14,7 +18,8 @@ client.on('message', (message) => {
     
     //!ban 
     //@cedricjake2006
-    const commandAndArguements = message.content.split(' ')
+    const args = message.content.slice(prefix.length).split(/ +/)
+    const command = args.shift().toLowerCase()
 
 })
 
